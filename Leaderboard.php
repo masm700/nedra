c3e543969cef19730dd3b93116c107cc853430ea
masm700 <82884095+masm700@users.noreply.github.com>
2022-11-12 21:48:41 -0800

    g

---
+++ Leaderboard.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <title>NEDRA GAME COMPANY</title>
+    <link href="assets/images/icon.png" rel="icon" type="image/png"/>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-cyborg-gaming.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+<!--
+
+TemplateMo 579 Cyborg Gaming
+
+https://templatemo.com/tm-579-cyborg-gaming
+
+-->
+  </head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.php" class="logo">
+                        <img src="assets/images/logo.png" alt="">
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                        <li><a href="index.php" class="active">Home</a></li>
+                        <li><a href="https://nedra-game-company.gitbook.io/untitled/">Whitepaper</a></li>
+                        <li><a href="Leaderboard.php">Leaderboard</a></li>
+                        <li><a href="Download.php">Download</a></li>
+                        <li><a style="cursor:pointer;" >Connect  <img src="assets/images/profile-header.jpg" alt=""></a></li>
+                    </ul>
+                    <a class='menu-trigger'>
+                      <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="page-content">
+        <div class="most-popular">
+          <div style="text-align:center;" class="col-lg-49">
+              <div class="heading-section">
+                <h4>LEADERBOARD</h4>
+              </div>
+              <table>
+                <tr>
+                  <th class="rank" >1. </th>
+                  <th class="username" >homyttm</th>
+                  <th class="times" >2:07:64</th>
+                </tr>
+                <br>    
+              </table>
+              <th><h4><p class="fsf" >This page is not supported for your device size  :(</p></h4></th>
+            </div>
+          </div>
+          <div class="gaming-library">
+            <div class="col-lg-12">
+              <div class="heading-section">
+                <h4>Our social pages</h4>
+              </div>
+              <div class="row">
+                <!------------------->
+                <div class="col-lg-3 col-sm-6">
+                  <div class="item">
+                    <span><a href="your address instagram"><img style="width:50px;" src="assets/images/instagram.png"><span style="color:white;padding-left:10px" >Instagram</span></a></span>
+                  </div>
+                </div>
+                <!------------------>
+                <div class="col-lg-3 col-sm-6">
+                  <div class="item">
+                    <span><a href="your address twiter"><img style="width:60px;" src="assets/images/twitter.png"><span style="color:white;padding-left:10px" >Twitter</span></a></span>
+                  </div>
+                </div>
+                <!------------------>
+                <div class="col-lg-3 col-sm-6">
+                  <div class="item">
+                    <span><a href="your address telegram"><img style="width:50px;" src="assets/images/telegram.png"><span style="color:white;padding-left:10px" >Telegram</span></a></span>
+                  </div>
+                </div>
+                <!------------------>
+                <!------------------>
+                <div class="col-lg-3 col-sm-6">
+                  <div class="item">
+                    <span><a href="your address discord"><img style="width:65px;" src="assets/images/discord.png"><span style="color:white;padding-left:10px" >Discord</span></a></span>
+                  </div>
+                </div>
+                <!------------------>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <p>Copyright © 2022 <a>Nedra Gaming</a> Company. All rights reserved. </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+  <script src="assets/js/isotope.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/tabs.js"></script>
+  <script src="assets/js/popup.js"></script>
+  <script src="assets/js/custom.js"></script>
+
+
+  </body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <title>NEDRA GAME COMPANY</title>
+    <link href="assets/images/icon.png" rel="icon" type="image/png"/>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-cyborg-gaming.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+<!--
+
+TemplateMo 579 Cyborg Gaming
+
+https://templatemo.com/tm-579-cyborg-gaming
+
+-->
+  </head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.php" class="logo">
+                        <img src="assets/images/logo.png" alt="">
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                        <li><a href="index.php" class="active">Home</a></li>
+                        <li><a href="https://nedra-game-company.gitbook.io/untitled/">Whitepaper</a></li>
+                        <li><a href="Leaderboard.php">Leaderboard</a></li>
+                        <li><a href="Download.php">Download</a></li>
+                        <li><a style="cursor:pointer;" >Connect  <img src="assets/images/profile-header.jpg" alt=""></a></li>
+                    </ul>
+                    <a class='menu-trigger'>
+                      <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="page-content">
+
+          <!-- ***** Banner Start ***** -->
+          <div class="main-banner">
+            <div class="row">
+              <div class="col-lg-7">
+                <div class="header-text">
+                  <h6 style="color:#ffffff;" >Welcome To NedraGame</h6>
+                  <h4 style="color:rgb(255, 225, 0);" ><em>Dans Of Car</em> Play To Earn Game</h4>
+                  <div class="main-button">
+                    <a style="background-color:#64605f;" href="Download.php">Play Now</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- ***** Banner End ***** -->
+
+          <!-- ***** Most Popular Start ***** -->
+          <div class="most-popular">
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="heading-section">
+                  <h4>Road Map</h4>
+                </div>
+                <div class="row">
+                  <div class="col-lg-3 col-sm-6">
+                    <div class="item">
+                      <span style="font-size:27px;color:white;" >Q3 2022</span>
+                      <span style="font-size:20px;color:white;" >  .  version alpha game</span>
+                      <span style="font-size:20px;color:white;" >  .  play to earn</span>
+                      <span style="font-size:20px;color:white;" >  .  smart contract</span>
+                      <span style="font-size:18px;color:white;" >  .  listed token in exchange</span>
+                    </div>
+                  </div>
+                  <!---------------------------------->
+                  <div class="col-lg-3 col-sm-6">
+                    <div class="item">
+                      <span style="font-size:27px;color:white;" >Q4 2022</span>
+                      <span style="font-size:20px;color:white;" >  .  version alpha game</span>
+                      <span style="font-size:20px;color:white;" >  .  play to earn</span>
+                      <span style="font-size:20px;color:white;" >  .  smart contract</span>
+                      <span style="font-size:18px;color:white;" >  .  listed token in exchange</span>
+                    </div>
+                  </div>
+                  <!---------------------------------->
+                  <div class="col-lg-3 col-sm-6">
+                    <div class="item">
+                      <span style="font-size:27px;color:white;" >Q1 2023</span>
+                      <span style="font-size:20px;color:white;" >  .  version alpha game</span>
+                      <span style="font-size:20px;color:white;" >  .  play to earn</span>
+                      <span style="font-size:20px;color:white;" >  .  smart contract</span>
+                      <span style="font-size:18px;color:white;" >  .  listed token in exchange</span>
+                    </div>
+                  </div>
+                  <!---------------------------------->
+                  <div class="col-lg-3 col-sm-6">
+                    <div class="item">
+                      <span style="font-size:27px;color:white;" >Q2 2023</span>
+                      <span style="font-size:20px;color:white;" >  .  version alpha game</span>
+                      <span style="font-size:20px;color:white;" >  .  play to earn</span>
+                      <span style="font-size:20px;color:white;" >  .  smart contract</span>
+                      <span style="font-size:18px;color:white;" >  .  listed token in exchange</span>
+                    </div>
+                  </div>
+                  <!---------------------------------->
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- ***** Most Popular End ***** -->
+
+          <!-- ***** Gaming Library Start ***** -->
+          <div class="gaming-library">
+            <div class="col-lg-12">
+              <div class="heading-section">
+                <h4>Our social pages</h4>
+              </div>
+              <div class="row">
+                <!------------------->
+                <div class="col-lg-3 col-sm-6">
+                  <div class="item">
+                    <span><a href="your address instagram"><img style="width:50px;" src="assets/images/instagram.png"><span style="color:white;padding-left:10px" >Instagram</span></a></span>
+                  </div>
+                </div>
+                <!------------------>
+                <div class="col-lg-3 col-sm-6">
+                  <div class="item">
+                    <span><a href="your address twiter"><img style="width:60px;" src="assets/images/twitter.png"><span style="color:white;padding-left:10px" >Twitter</span></a></span>
+                  </div>
+                </div>
+                <!------------------>
+                <div class="col-lg-3 col-sm-6">
+                  <div class="item">
+                    <span><a href="your address telegram"><img style="width:50px;" src="assets/images/telegram.png"><span style="color:white;padding-left:10px" >Telegram</span></a></span>
+                  </div>
+                </div>
+                <!------------------>
+                <!------------------>
+                <div class="col-lg-3 col-sm-6">
+                  <div class="item">
+                    <span><a href="your address discord"><img style="width:65px;" src="assets/images/discord.png"><span style="color:white;padding-left:10px" >Discord</span></a></span>
+                  </div>
+                </div>
+                <!------------------>
+              </div>
+            </div>
+          </div>
+          <!-- ***** Gaming Library End ***** -->
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <p>Copyright © 2022 <a>Nedra Gaming</a> Company. All rights reserved. </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+  <script src="assets/js/isotope.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/tabs.js"></script>
+  <script src="assets/js/popup.js"></script>
+  <script src="assets/js/custom.js"></script>
+
+
+  </body>
+
+</html>
